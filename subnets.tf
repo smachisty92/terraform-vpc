80e3958d78de50336c05d4e463dcf8765c3b9bb9
csyed <smachisty92@gmail.com>
2022-03-03 16:58:08 +0530

    terraform

--- subnets.tf
+++ subnets.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "public-subnet" {
   cidr_block = cidrsubnet(var.VPC_CIDR,3,count.index)
   availability_zone = element(var.AZ, count.index )
   tags = {
-    Name = "public-subnet-${count.index}"
+    Name = "public-subnet-${count.index+1}"
   }
 }
 
@@ -13,6 +13,9 @@ resource "aws_subnet" "private-subnet" {
   vpc_id = aws_vpc.main.id
   cidr_block = cidrsubnet(var.VPC_CIDR,3,count.index + 2)
   availability_zone = element(var.AZ, count.index )
+  tags = {
+    Name = "private-subnet-${count.index+1}"
+  }
 }
 
 
