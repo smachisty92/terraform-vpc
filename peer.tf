956685c4ca7391b343d6fddf4f0079961fcd8466
csyed <smachisty92@gmail.com>
2022-03-03 13:08:26 +0530

    terraform

--- peer.tf
+++ peer.tf
@@ -1,4 +1,5 @@
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peer-to-default-vpc" {
   peer_vpc_id = var.DEFAULT_VPC_ID
   vpc_id      = aws_vpc.main.id
+  auto_accept = true
 }
