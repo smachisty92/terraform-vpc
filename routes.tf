46903c2c4bf910b5ad1eab27b9212ce01db6468d
csyed <smachisty92@gmail.com>
2022-03-03 14:36:37 +0530

    terraform

--- routes.tf
+++ routes.tf
@@ -6,7 +6,7 @@ resource "aws_route_table" "route-table" {
 }
 
 resource "aws_route" "route-to-default-vpc"{
-  route_table_id = aws_route.route-to-default-vpc.id
+  route_table_id = aws_route_table.route-table.id
   destination_cidr_block =var.DEFAULT_VPC_CIDR
   vpc_peering_connection_id = aws_vpc_peering_connection.peer-to-default-vpc.id
 }
