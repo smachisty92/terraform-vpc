5ec47ac335e236eeb8e73c450fba2c46f8f7ad83
csyed <smachisty92@gmail.com>
2022-03-03 19:38:55 +0530

    terraform

--- routes.tf
+++ routes.tf
@@ -36,3 +36,10 @@ resource "aws_route" "route-to-internetgateway"{
   destination_cidr_block ="0.0.0.0/0"
   gateway_id = aws_internet_gateway.gw.id
 }
+
+
+resource "aws_route" "route-to-natgataway"{
+  route_table_id = aws_route_table.private-route-table.id
+  destination_cidr_block ="0.0.0.0/0"
+  gateway_id = aws_nat_gateway.ngw.id
+}
