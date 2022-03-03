cb66eb8ca1c56d250a2ae4c5c23af9c2a474578f
csyed <smachisty92@gmail.com>
2022-03-03 16:14:31 +0530

    terraform

--- output.tf
+++ output.tf
@@ -6,10 +6,18 @@ output "VPC_CIDR" {
   value = aws_vpc.main.cidr_block
 }
 
-output "SUBNETS_ID" {
-  value = aws_subnet.subnet.*.id
+output "PUBLIC-SUBNETS_ID" {
+  value = aws_subnet.public-subnet.*.id
 }
 
-output "SUBNETS_CIDR" {
-  value = aws_subnet.subnet.*.cidr_block
+output "PUBLIC-SUBNETS_CIDR" {
+  value = aws_subnet.public-subnet.*.cidr_block
+}
+
+output "PRIVATE-SUBNETS_ID" {
+  value = aws_subnet.private-subnet.*.id
+}
+
+output "PRIVATE-SUBNETS_CIDR" {
+  value = aws_subnet.private-subnet.*.cidr_block
 }
--- subnets.tf
+++ subnets.tf
@@ -1,12 +1,27 @@
-resource "aws_subnet" "subnet" {
-  count = var.SUBNETS
+resource "aws_subnet" "public-subnet" {
+  count = var.PUBLIC_SUBNETS
   vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(var.VPC_CIDR,1,count.index)
+  cidr_block = cidrsubnet(var.VPC_CIDR,3,count.index)
   availability_zone = element(var.AZ, count.index )
 }
 
-resource "aws_route_table_association" "rt-association" {
-  count = var.SUBNETS
-  subnet_id = element(aws_subnet.subnet.*.id, count.index)
-  route_table_id = aws_route_table.route-table.id
+resource "aws_subnet" "private-subnet" {
+  count = var.PRIVATE_SUBNETS
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.VPC_CIDR,3,count.index+2)
+  availability_zone = element(var.AZ, count.index )
 }
+
+
+resource "aws_route_table_association" "rt-association-public" {
+  count = var.PUBLIC_SUBNETS
+  subnet_id = element(aws_subnet.public-subnet.*.id, count.index)
+  route_table_id = aws_route_table.public-route-table.id
+}
+resource "aws_route_table_association" "rt-association-private" {
+  count = var.PRIVATE_SUBNETS
+  subnet_id = element(aws_subnet.private-subnet.*.id, count.index)
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+
